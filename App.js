b45f375d0f0141c6c28a0ed20393b888fbdcf802
Jéssica Torres de Lima <70148001+jessicatorreslima@users.noreply.github.com>
2022-07-25 18:45:40 -0300

    Inclui detecção de shake

--- App.js
+++ App.js
@@ -1,9 +1,26 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import Torch from 'react-native-torch';
+import RNShake from 'react-native-shake';
 
 const App = () => {
   const [switcher, setSwitcher] = useState(false);
   const handleSwitcher = () => setSwitcher(oldSwitcher => !oldSwitcher);
+
+  useEffect(() => {
+    // Liga lanterna do celular
+    Torch.switchState(switcher);
+  }, [switcher]);
+
+  useEffect(() => {
+    // Muda o switcher quando o celular for chacoalhado
+    const subscription = RNShake.addListener(() => {
+      handleSwitcher(oldSwitcher => !oldSwitcher);
+    });
+
+    // Essa função será chamada quando o componente for desmontado
+    return () => subscription.remove();
+  }, []);
 
   return (
     <View
